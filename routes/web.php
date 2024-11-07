f5feb610695af64dda03d50ec7ae238c66a88a9c
Prateek <prateek45698@gmail.com>
2024-11-07 11:15:06 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
+
+// Redirect root URL to login for guests, home for authenticated users
+Route::get('/', function () {
+    return auth()->check() ? redirect('/home') : redirect('/login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+    // Todo Routes
+    Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+    Route::patch('/todos/{todo}', [TodoController::class, 'toggle'])->name('todos.toggle');
+    Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
+});
